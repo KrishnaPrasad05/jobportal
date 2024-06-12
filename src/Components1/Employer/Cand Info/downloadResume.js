d9ed22b8590d3a0d0d09a0301dd1b0e3045bfc32
KrishnaPrasad05 <krishnasrinivasan41@gmail.com>
2024-06-12 13:27:28 +0530

    Correction

--- src/Components1/Employer/Cand Info/downloadResume.js
+++ src/Components1/Employer/Cand Info/downloadResume.js
@@ -112,7 +112,7 @@ function CandInfoPDF() {
             <nav aria-label="breadcrumb " style={{backgroundColor: '#E6E7FF',paddingLeft: '10px',top:'3.8rem',fontWeight: 500,fontSize: '16px',position:'fixed',width:'100%',zIndex:1000,height:'30px'}}>
                 <ol class="breadcrumb p-1">
                   <li class="breadcrumb-item" aria-current="page"><Link to="/jobseeker-home" style={{textDecoration:'none',color:'#2B358C'}}>Home</Link></li>
-                  <li class="breadcrumb-item" aria-current="page"><Link to="/peofile" style={{textDecoration:'none',color:'#2B358C'}}>Profile</Link></li>
+                  <li class="breadcrumb-item" aria-current="page"><Link to="/profilejs" style={{textDecoration:'none',color:'#2B358C'}}>Profile</Link></li>
                   <li class="breadcrumb-item active" aria-current="page">Download resume</li>
                 </ol>
               </nav>
